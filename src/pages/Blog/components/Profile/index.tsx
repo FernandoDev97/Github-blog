77fa4b161ba014df9a339e5e46b1bac81b453091
Fernando Souza <l.fernando.dev@gmail.com>
2023-04-11 00:01:32 -0300

    up config and fist components github blog

---
+++ src/pages/Blog/components/Profile/index.tsx
@@ -0,0 +1,12 @@
+import { ProfileContainer } from './styles';
+
+interface ProfileProps {
+}
+
+export function Profile() {
+  return (
+    <ProfileContainer>
+      
+    </ProfileContainer>
+  );
+}
