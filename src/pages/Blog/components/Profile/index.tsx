57153e4023910506782e33d594459d3e9620cec3
Fernando Souza <l.fernando.dev@gmail.com>
2023-04-12 23:38:52 -0300

    add styles from post to home page

--- src/pages/Blog/components/Profile/index.tsx
+++ src/pages/Blog/components/Profile/index.tsx
@@ -16,8 +16,7 @@ export function Profile() {
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
+          minim veniam.
         </p>
         <ul>
           <li>
--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import { Profile } from './components/Profile'
+import { SearchInput } from './components/SearchInput'
+import { PostsListContainer } from './styles'
+import { Post } from './components/Post'
 
 export const Blog = () => {
   return (
     <>
       <Profile/>
+      <SearchInput/>
+      <PostsListContainer>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+      </PostsListContainer>
     </>
   )
 }
